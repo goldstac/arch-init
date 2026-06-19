c4090246014345f07bb548b0475c93d40d64bb4a
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-06-20 01:04:54 +0400

    Add system update and package installation script
    This script updates the system, installs essential packages, and sets up yay for package management.

--- src/templates-shell/base.sh
+++ src/templates-shell/base.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+
+echo "updating system"
+
+sudo pacman -Syu
+
+echo "installing git vs code base-devel and fastfetch"
+
+sudo pacman -S git code base-devel fastfetch
+
+echo "installing yay"
+
+git clone https://github.com/Jguer/yay.git
+
+cd yay
+
+makepkg -si
+
+echo "installing cursor"
+
+yay -S cursor-bin
 
