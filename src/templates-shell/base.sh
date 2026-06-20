b732dfc6104fab8361fb09299ddc105d1930ed82
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-06-20 11:58:19 +0400

    Update base.sh for non-interactive package installation
    Added --noconfirm option to pacman and makepkg commands for non-interactive installation.

--- src/templates-shell/base.sh
+++ src/templates-shell/base.sh
@@ -2,17 +2,17 @@
 
 echo "updating system"
 
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
 echo "installing git vs code base-devel and fastfetch"
 
-sudo pacman -S git code base-devel fastfetch
+sudo pacman -S git code base-devel fastfetch --noconfirm
 
 echo "installing yay"
 
-git clone https://github.com/Jguer/yay.git
+git clone https://aur.archlinux.org/yay.git
 
 cd yay
 
-makepkg -si
+makepkg -si --noconfirm
 cd ..
