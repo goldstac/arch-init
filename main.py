218b7c68980092caa813804033a17681cdc38b80
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-06-20 11:34:35 +0400

    Add template selection prompt and base template option

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import subprocess
 print("Welcome To Arch Init | By Li Productions")
 print("CHOOSE A TEMPLATE :")
+print("-------------------------")
 print("BASE : 1")
 template_choose = input("CHOOSE TEMPLATE : ")
 if template_choose == "1":
     subprocess.run("chmod +x templates-shell/base.sh", shell=True)
     subprocess.run("templates-shell/./base.sh",shell=True)
-
 
