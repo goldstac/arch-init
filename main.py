05cd834564009652e3392c07c349ec81dcf88cc4
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-06-20 11:58:44 +0400

    Fix path for base.sh in main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import subprocess
+base_location = "src/templates-shell/base.sh"
 print("Welcome To Arch Init | By Li Productions")
 print("CHOOSE A TEMPLATE :")
 print("-------------------------")
 print("BASE : 1")
+print("This Installs, Git,YAY,Fastfetch,BASE-DEVEL,VS CODE")
+print("This Installs A Community Version OF VS CODE Or If You Are On GNOME You CAN Just Download The Flathub Version In The GNOME APP STORE")
+print(f"This is gonna run base.sh located at {base_location}")
 template_choose = input("CHOOSE TEMPLATE : ")
 if template_choose == "1":
-    subprocess.run("chmod +x templates-shell/base.sh", shell=True)
-    subprocess.run("templates-shell/./base.sh",shell=True)
+    subprocess.run("chmod +x src/templates-shell/base.sh", shell=True)
+    subprocess.run("src/templates-shell/./base.sh",shell=True)
 
